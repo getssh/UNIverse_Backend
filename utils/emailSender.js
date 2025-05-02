33abcdab692ee6c2a6cc11dbf03f24417c490f8d
getssh <gtamrat33@gmail.com>
2025-05-02 10:19:34 +0300

    Update email service to gmail

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -3,9 +3,9 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
 
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: parseInt(process.env.EMAIL_PORT || '587', 10),
-
+        // host: process.env.EMAIL_HOST,
+        // port: parseInt(process.env.EMAIL_PORT || '587', 10),
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER, 
             pass: process.env.EMAIL_PASS,
