f6461a9ad8b90f90074b636e3613357853f1302b
getssh <gtamrat33@gmail.com>
2025-06-02 00:53:34 +0300

    Update group kick url for apidoc

--- routes/apiDocsRoutes.js
+++ routes/apiDocsRoutes.js
@@ -16,15 +16,17 @@ const universitySwaggerDocument = loadSwaggerDocument('university.yaml');
 const channelSwaggerDocument = loadSwaggerDocument('channel.yaml');
 const userSwaggerDocument = loadSwaggerDocument('user.yaml');
 const commentsAndReportsSwaggerDocument = loadSwaggerDocument('commentAndReport.yaml');
+const eventSwaggerDocument = loadSwaggerDocument('event.yaml')
 
 
 router.use('/universities', swaggerUi.serve, swaggerUi.setup(universitySwaggerDocument));
 router.use('/users', swaggerUi.serve, swaggerUi.setup(userSwaggerDocument));
 router.use('/channels', swaggerUi.serve, swaggerUi.setup(channelSwaggerDocument));
-router.use('/groups', swaggerUi.serve, swaggerUi.setup(groupSwaggerDocument));
 router.use('/posts', swaggerUi.serve, swaggerUi.setup(postSwaggerDocument));
 router.use('/chats', swaggerUi.serve, swaggerUi.setup(chatSwaggerDocument));
 router.use('/messages', swaggerUi.serve, swaggerUi.setup(messageSwaggerDocument));
 router.use('/commentsAndReports', swaggerUi.serve, swaggerUi.setup(commentsAndReportsSwaggerDocument));
+router.use('/groups', swaggerUi.serve, swaggerUi.setup(groupSwaggerDocument));
+router.use('/events', swaggerUi.serve, swaggerUi.setup(eventSwaggerDocument));
 
 module.exports = router;
